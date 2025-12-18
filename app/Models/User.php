6b938235cce9d871274cb3df7cf683c8201e01be
Deri Prasetyo <deri.mbois@gmail.com>
2025-12-18 16:10:17 +0700

    Add ObservedBy attribute to User model for observer integration

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
+#[ObservedBy(\App\Observers\UserObserver::class)]
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
